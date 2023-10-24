d141bffb5e0507d098480d4a3047c7f2d35ad60c
Young-Gaius <youngehu8@gmail.com>
2023-10-24 23:25:54 +0100

    add counter

---
+++ script.js
@@ -0,0 +1,10 @@
+let count = 0
+
+output = document.getElementById("number")
+save = document.getElementById("save")
+para = document.getElementById("p-e")
+
+function increment () {
+    count += 1
+    output.innerText = count
+} 
